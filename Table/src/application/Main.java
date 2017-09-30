adbd9835c373bf5822ce29b18ada5179de0ef356
Миша <@192.168.1.202>
2017-09-30 20:25:26 +0300

    f

--- Table/src/application/Main.java
+++ Table/src/application/Main.java
@@ -1,42 +1,57 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	
-	private Stage stage;
+	protected Stage stage;
 	private Scene scene;
 	private Pane pane;
 	
 	@FXML
 	private Button btn;
-	
+	@FXML
+	private TextField txt1; 
+	@FXML
+	private TextField txt2;
 	public void start(Stage stage) {
 		this.stage = stage;
 		try {
-			pane = (Pane) FXMLLoader.load(Main.class.getResource("писька.fxml"));
+			pane = (Pane) FXMLLoader.load(Main.class.getResource(".fxml"));
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		scene = new Scene(pane, 640, 480);
+		scene = new Scene(pane, 555, 211);
 		stage.setScene(scene);
 		
 		stage.show();
 	}
 	
-	@FXML
-	public void initialize() {
+	public void umnichka(){
+		txt1.getText();
 		
 	}
 	
+	@FXML
+	public void initialize() {
+		btn.setOnAction (new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event){
+				umnichka();	
+			}
+		});
+		
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
